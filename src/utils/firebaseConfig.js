f411988ded3a6748c304511f658b28a0e5b76c96
Shobhit Gautam <mk42shobhit.g@gmail.com>
2025-06-28 04:25:30 +0530

    Fixed GEMINI API KEY Exposure

--- src/utils/firebaseConfig.js
+++ src/utils/firebaseConfig.js
@@ -2,13 +2,14 @@
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
+import { GEMINI_API_KEY } from "./constants";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDtTsFV9-qeq8dPWX_1i3o2M8GbvqLUtOI",
+  apiKey: GEMINI_API_KEY,
   authDomain: "flixipedia-c7a6d.firebaseapp.com",
   projectId: "flixipedia-c7a6d",
   storageBucket: "flixipedia-c7a6d.firebasestorage.app",
